716e87cf8b516d28bb1331b58b00eb3f59e8f57a
Shaladar79 <scott_d_anderson79@yahoo.com>
2026-02-08 11:12:36 -0600

    Add configuration for Deadlands Classic system

---
+++ scripts/config.mjs
@@ -0,0 +1,18 @@
+export const DLC = {
+  SYSTEM_ID: "deadlands-classic",
+  SYSTEM_NAME: "Deadlands Classic",
+
+  ACTOR_TYPES: {
+    pc: "PC",
+    npc: "NPC"
+  },
+
+  ITEM_TYPES: {
+    gear: "Gear",
+    edge: "Edge",
+    hindrance: "Hindrance",
+    weapon: "Weapon",
+    aptitude: "Aptitude",
+    power: "Power"
+  }
+};
